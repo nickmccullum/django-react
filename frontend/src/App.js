e46a9bee2c434e96e66619d983197369b2e76fac
Nick McCullum <nicholasmccullum@gmail.com>
2021-04-03 09:20:24 -0300

    added the Container component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import { Container } from 'react-bootstrap'
 function App() {
   return (
     <div>
       <Header />
         <main>
+            <Container>
             <h1>Welcome</h1>
+            </Container>
         </main>
       <Footer />
     </div>

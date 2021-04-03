5a5c614cf252413ec65866bdcfd1f8c28076267d
Nick McCullum <nicholasmccullum@gmail.com>
2021-04-03 09:29:29 -0300

    added a navbar to the Header.js component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import { Navbar, Nav, Container, Row } from 'react-bootstrap'
 function Header() {
     return (
         <div>
             <header>
-                Header
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
             </header>
         </div>
     )

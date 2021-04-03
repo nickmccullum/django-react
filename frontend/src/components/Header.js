11fbf1210b0a3b5499c57db071e949aa80dc0295
Nick McCullum <nicholasmccullum@gmail.com>
2021-04-03 09:33:22 -0300

    formatted the navbar in Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,15 +4,18 @@ function Header() {
     return (
         <div>
             <header>
-                <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
+                <Navbar bg="dark" variant=
+                    "dark" expand="lg" collapseOnSelect>
+                    <Container>
+                        <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                                <Nav.Link href="/cart">Cart</Nav.Link>
+                                <Nav.Link href="/login">Login</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
                 </Navbar>
             </header>
         </div>

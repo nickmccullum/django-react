c7544a9d4f50a2399ab6e0726d1a80a2a64e37bf
Nick McCullum <nicholasmccullum@gmail.com>
2021-04-03 09:13:04 -0300

    added unstyled Header and Footer components to the App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
-      My App
+      <Header />
+        <main>
+            <h1>Welcome</h1>
+        </main>
+      <Footer />
     </div>
   );
 }
